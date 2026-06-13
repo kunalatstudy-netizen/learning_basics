29450014a8269de61e991e0b646b5e4cae53b83a
Kunal Rai <kunalatstudy@gmail.com>
2026-06-13 11:47:11 +0530

    learned about variables and print

---
+++ learning_print.py
@@ -0,0 +1,45 @@
+# # working with varialbles and print function
+#
+# print("Hello World")
+#
+# print("Day1 - Python print function")
+# print("The function is declared like this: ")
+# print("Print ('what to print')")
+#
+# print("Hello World\nWhat is your name\nwhere are you from")
+# print("hello" + "world")
+# print("What is your name\nwhere are you from")
+#
+# # adding space in string concetination
+#
+# print("hello" + " " + "world")
+#
+# # the input function
+#
+# print("Hello" + input("What is your name") + "!")
+#
+# # create a program to print the length of the name
+#
+#
+# print(len(input("What is your name")))
+#
+# name = input("What is your name")
+# print(len(name))
+
+# write a program that switches the values store in variable a & b
+
+# a = input("a: ")
+# b = input("b: ")
+#
+# c = b
+# b = a
+# a = c
+# print(f"a: {a} b: {b}")
+
+# create a band name generator
+
+# print("Welcome to the Band name generator")
+#
+# city_name = input("What is the name of the city you grew up in? ")
+# pet_name = input("What is the name of the pet you grew up in? ")
+# print(f"Your band name {city_name}{pet_name}")
